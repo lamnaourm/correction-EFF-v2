7c13b96c57a2ae177273368d05de702543b4a31d
lamnaourm <mohammed.lamnaour@gmail.com>
2024-05-31 15:21:19 +0100

    q4

--- src/components/Verification.jsx
+++ src/components/Verification.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Verification() {
+
+  const {idDossier} = useParams()
+  const [res, setRes] = useState()
+  useEffect(() => {
+
+    const getData = async () => {
+      const res = axios.get(`http://127.0.0.1:8000/api/verifier/${idDossier}`)
+      return res.data
+    }
+    getData().then((r) => setRes(r))
+
+  }, [idDossier])
+
   return (
-    <div>Verification</div>
+    <div>
+      {res}
+    </div>
   )
 }
